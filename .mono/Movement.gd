a45552d34783977e2da8ecadac9912e4ac43f349
emb3rn <embern@protonmail.com>
2022-11-08 21:20:59 +0000

    Working camera movement

---
+++ .mono/Movement.gd
@@ -0,0 +1,25 @@
+extends Camera
+
+var speed_multiplier: int = 1
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		translate(Vector3(0, 0, -5 * delta))
+	
+	if Input.is_action_pressed("ui_down"):
+		translate(Vector3(0, 0, 5 * delta))
+		
+	if Input.is_action_pressed("ui_right"):
+		translate(Vector3(5 * delta, 0, 0))
+		
+	if Input.is_action_pressed("ui_left"):
+		translate(Vector3(-5 * delta, 0, 0))
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		print(event.position)
+
+func _ready():
+	print("Hello world!")
+
+
