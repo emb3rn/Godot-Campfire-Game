bea040659d45d62654459e10649d503f98396152
Oskar Durys <embern@protonmail.com>
2022-11-09 06:56:39 +0000

    Movement working, mouse movement next

--- .mono/Movement.gd
+++ .mono/Movement.gd
@@ -1,13 +1,10 @@
 extends Camera
 
 var speed_multiplier: int = 1
-
-func recent_event(p_event):
-	if p_event.pressed == p_event.scancode:
-		return p_event.pressed
+var mouse_position: Vector2
+var global_delta
 
 func _process(delta):
-	
 	if Input.is_action_pressed("ui_up"):
 		translate(Vector3(0, 0, -5 * delta))
 	
@@ -19,10 +16,21 @@ func _process(delta):
 		
 	if Input.is_action_pressed("ui_left"):
 		translate(Vector3(-5 * delta, 0, 0))
-
+	
+	global_delta = delta
+	
 func _input(event):
 	if event is InputEventMouseMotion:
-		print(event.position)
+		event.relative
+		print(clamp(rad2deg(event.relative.y), -90, 90))
+		#rotate_y(pos_change.y/5)
+		#rotate_x(pos_change.x/5)
+		
+		rotation.z = 0
+			
+func _unhandled_input(event):
+	if event is InputEventKey:
+		print(event.scancode)
 
 func _ready():
 	print("Hello world!")
